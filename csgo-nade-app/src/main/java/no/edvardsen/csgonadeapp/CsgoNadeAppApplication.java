17c62bd174b75074ad345c81f61e34a891831f04
jKm00 <joakimedvardsen2000@gmail.com>
2023-01-19 22:51:28 +0100

    Spring boot initialization

---
+++ csgo-nade-app/src/main/java/no/edvardsen/csgonadeapp/CsgoNadeAppApplication.java
@@ -0,0 +1,13 @@
+package no.edvardsen.csgonadeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsgoNadeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsgoNadeAppApplication.class, args);
+	}
+
+}
---
+++ csgo-nade-app/src/test/java/no/edvardsen/csgonadeapp/CsgoNadeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package no.edvardsen.csgonadeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsgoNadeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
