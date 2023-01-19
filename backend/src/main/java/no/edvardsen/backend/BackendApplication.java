78c7ff46b5743f436ab269b36f841972cc354c07
jKm00 <joakimedvardsen2000@gmail.com>
2023-01-19 23:13:13 +0100

    Another reinitialization of spring boot

--- backend/src/main/java/no/edvardsen/backend/BackendApplication.java
+++ backend/src/main/java/no/edvardsen/backend/BackendApplication.java
@@ -1,13 +1,13 @@
-package no.edvardsen.csgonades;
+package no.edvardsen.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CsgoNadesApplication {
+public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CsgoNadesApplication.class, args);
+		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
--- backend/src/test/java/no/edvardsen/backend/BackendApplicationTests.java
+++ backend/src/test/java/no/edvardsen/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package no.edvardsen.csgonades;
+package no.edvardsen.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CsgoNadesApplicationTests {
+class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
