71fcf98ef6d41d4e527640be9b7d833cc1d3e8ea
jKm00 <joakimedvardsen2000@gmail.com>
2023-01-19 22:52:16 +0100

    Deleted old project

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module nade.app {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-    requires java.management;
-    opens no.dot.csgo.nadeApp to javadx.fxml;
-    exports no.dot.csgo.nadeApp;
-}
