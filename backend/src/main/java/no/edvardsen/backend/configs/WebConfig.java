7e4c49412c44751507226310688572d670c89c09
Joakim <joakied@stud.ntnu.no>
2023-01-28 20:06:19 +0100

    fixed allowed orgins to api

--- backend/src/main/java/no/edvardsen/backend/configs/WebConfig.java
+++ backend/src/main/java/no/edvardsen/backend/configs/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 
   @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/api/**").allowedOrigins("https://csgostrats.com");
     }
   
 }
