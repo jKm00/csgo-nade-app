8a7ef9f76e8b9086f4c97e117f5602bacaba31be
jKm00 <joakimedvardsen2000@gmail.com>
2023-01-29 20:41:01 +0100

    added google analytics

--- sveltekit/src/routes/+layout.svelte
+++ sveltekit/src/routes/+layout.svelte
@@ -1,28 +1,38 @@
 <script lang="ts">
-  import './styles.css';
+	import './styles.css';
 </script>
 
 <svelte:head>
-  <title>CSGO Strats</title>
-  <link rel="icon" type="image/svg" href="assets/favicons/favicon.ico" />
+	<title>CSGO Strats</title>
+	<link rel="icon" type="image/svg" href="assets/favicons/favicon.ico" />
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-NX89CG2FN7"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+
+		gtag('config', 'G-NX89CG2FN7');
+	</script>
 </svelte:head>
 
 <main class="app">
-  <slot />
+	<slot />
 </main>
 
 <style scoped>
-  .app {
-    width: clamp(500px, 90vw, 1000px);
-    margin: 2rem auto;
-    padding-inline: 1rem;
-    text-align: center;
-  }
+	.app {
+		width: clamp(500px, 90vw, 1000px);
+		margin: 2rem auto;
+		padding-inline: 1rem;
+		text-align: center;
+	}
 
-  main {
-    display: flex;
-    flex-direction: column;
-    gap: 1rem;
-    width: 100%;
-  }
+	main {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+		width: 100%;
+	}
 </style>
