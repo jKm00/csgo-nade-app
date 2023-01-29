420e6aa0fd4ac13c02f94a7bbef9713e132dcbff
jKm00 <joakimedvardsen2000@gmail.com>
2023-01-29 16:29:59 +0100

    Removed old frontend

--- frontend/src/types/CsgoMap.ts
+++
@@ -1,9 +0,0 @@
-import type { Strat } from "@/types/Strat";
-
-export type CsgoMap = {
-  name: string;
-  thumbnail: string;
-  radar: string;
-  available: boolean;
-  strats: Strat[];
-}
--- frontend/src/types/Strat.ts
+++
@@ -1,7 +0,0 @@
-import type { Lineup } from "@/types/Lineup";
-
-export type Strat = {
-  id: number;
-  name: string;
-  lineups: Lineup[]
-}
