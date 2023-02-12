2009458b45418981baff28381f14d31196d2e38e
jKm00 <joakimedvardsen2000@gmail.com>
2023-02-12 02:55:23 +0100

    add home navigation to login page

--- sveltekit/src/routes/login/+page.svelte
+++ sveltekit/src/routes/login/+page.svelte
@@ -24,7 +24,16 @@
 
 <section class="auth-section">
 	<div class="content">
-		<h1 class="title">Login</h1>
+		<header class="header">
+			<a href="/" class="back"
+				><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"
+					><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path
+						d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 278.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"
+					/></svg
+				>Home</a
+			>
+			<h1 class="title">Login</h1>
+		</header>
 		<form class="form" on:submit|preventDefault={handleSubmit}>
 			<div class="input-wrapper">
 				<label for="username" class="label" data-active={usernameActive}>Username</label>
@@ -88,7 +97,29 @@
 		display: grid;
 		gap: 3rem;
 		align-content: center;
-		padding-inline: 5rem;
+		padding-inline: 4rem;
+	}
+
+	.header {
+		display: grid;
+		grid-template-columns: 20% 1fr 20%;
+		align-items: center;
+	}
+
+	.back {
+		display: flex;
+		gap: 0.25rem;
+		align-items: center;
+	}
+
+	.back svg {
+		fill: #fff;
+		height: 1rem;
+	}
+
+	.back:hover svg,
+	.back:focus-visible svg {
+		fill: var(--clr-accent);
 	}
 
 	.form {
