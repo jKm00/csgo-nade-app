2ac8b5b2e36aded43194650bac13717e3533ffe2
jKm00 <joakimedvardsen2000@gmail.com>
2023-05-16 23:07:46 +0200

    wip: general profile settings

---
+++ src/routes/(app)/users/[uuid]/settings/+layout.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	export let data;
+
+	$: ({ session } = data);
+</script>
+
+<div class="grid gap-4 w-default px-4" style="grid-template-columns: 10rem 1fr">
+	<aside>
+		<a
+			class="flex items-center gap-2 text-sm mb-5 underline"
+			href="/users/{session?.user.id}"
+			><svg
+				class="w-2 fill-white"
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 320 512"
+				><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
+					d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"
+				/></svg
+			>Back to profile</a
+		>
+		<h2 class="text-neutral-400 font-bold uppercase text-sm mb-2">General</h2>
+		<ul>
+			<li>
+				<a
+					class="hover:underline focus-within:underline"
+					href="/users/{session?.user.id}/settings">Profile settings</a
+				>
+			</li>
+			<li>
+				<a
+					class="hover:underline focus-within:underline"
+					href="/users/{session?.user.id}/settings/teams">Team settings</a
+				>
+			</li>
+		</ul>
+	</aside>
+	<main>
+		<slot><!-- optional fallback --></slot>
+	</main>
+</div>
--- src/routes/(app)/users/[uuid]/settings/+page.svelte
+++ src/routes/(app)/users/[uuid]/settings/+page.svelte
@@ -1,5 +1,95 @@
 <script lang="ts">
 	export let data;
+
+	$: ({ user } = data);
 </script>
 
-<p>Settings page for: {data.uuid}</p>
+<div />
+<form action="" class="grid gap-4 max-w-sm m-auto mb-16">
+	<div class="grid">
+		<label class="font-bold" for="name">Name:</label>
+		<input
+			class="bg-neutral-800 rounded-sm p-2"
+			type="text"
+			id="name"
+			name="name"
+			placeholder={user?.name}
+		/>
+	</div>
+	<div class="grid">
+		<label class="font-bold" for="username">Username:</label>
+		<input
+			class="bg-neutral-800 rounded-sm p-2"
+			type="text"
+			id="username"
+			name="username"
+			placeholder={user?.username}
+		/>
+	</div>
+	<button
+		class="bg-green-400 px-2 py-1 rounded justify-self-end text-neutral-950 text-sm"
+		type="submit">Save changes</button
+	>
+</form>
+
+<form action="" class="grid gap-4 max-w-sm m-auto mb-16">
+	<div class="grid">
+		<label class="font-bold" for="email">Email:</label>
+		<input
+			class="bg-neutral-800 rounded-sm p-2"
+			type="text"
+			id="email"
+			name="email"
+			placeholder={user?.email}
+		/>
+	</div>
+	<button
+		class="bg-green-400 px-2 py-1 rounded justify-self-end text-neutral-950 text-sm"
+		type="submit">Change email</button
+	>
+</form>
+
+<form action="" class="grid gap-4 max-w-sm m-auto mb-16">
+	<div class="grid">
+		<label class="font-bold" for="currentPassword">Current password:</label>
+		<input
+			class="bg-neutral-800 rounded-sm p-2"
+			type="password"
+			id="currentPassword"
+			name="currentPassword"
+			placeholder="********"
+		/>
+	</div>
+	<div class="grid">
+		<label class="font-bold" for="newPassword">New password:</label>
+		<input
+			class="bg-neutral-800 rounded-sm p-2"
+			type="password"
+			id="newPassword"
+			name="newPassword"
+			placeholder="********"
+		/>
+	</div>
+	<div class="grid">
+		<label class="font-bold" for="confirmPassword">Confirm password:</label>
+		<input
+			class="bg-neutral-800 rounded-sm p-2"
+			type="password"
+			id="confirmPassword"
+			name="confirmPassword"
+			placeholder="********"
+		/>
+	</div>
+	<button
+		class="bg-green-400 px-2 py-1 rounded justify-self-end text-neutral-950 text-sm"
+		type="submit">Change password</button
+	>
+</form>
+
+<form action="" class="grid gap-4 max-w-sm m-auto mb-16">
+	<h2 class="font-bold text-red-400">Danger zone</h2>
+	<button
+		class="bg-red-400/20 border-2 p-2 border-red-400 rounded text-red-400 hover:bg-red-400 hover:text-white focus-within:bg-red-400 focus-within:text-white transition-colors"
+		>Delete account</button
+	>
+</form>
