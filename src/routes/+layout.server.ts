1b7c93e0db95e8d4b140ef00b338f512b65deef3
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2023-05-29 19:09:20 +0200

    refactor user drop down menu

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,16 +1,7 @@
-import { redirect } from '@sveltejs/kit';
-import type { LayoutServerLoad } from './$types';
-
-export const load: LayoutServerLoad = async ({ locals }) => {
+export const load = async ({ locals }) => {
 	const session = await locals.getSession();
-
-	const { data } = await locals.supabase
-		.from('profiles')
-		.select()
-		.eq('uuid', session?.user?.id);
 
 	return {
 		session,
-		authUser: data !== null && data.length > 0 ? data[0] : null,
 	};
 };
