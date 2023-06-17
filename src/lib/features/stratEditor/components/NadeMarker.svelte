89a8c015ef2493a289e237755eefef793e87d18f
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2023-06-18 00:04:12 +0200

    fix: clicking on number in marker moved it

--- src/lib/features/stratEditor/components/NadeMarker.svelte
+++ src/lib/features/stratEditor/components/NadeMarker.svelte
@@ -79,7 +79,7 @@
 		><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
 			d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"
 		/></svg
-	><span class="absolute text-xs">{index + 1}</span></button
+	><span class="absolute text-xs pointer-events-none">{index + 1}</span></button
 >
 
 {#if nade.impactPointX && nade.impactPointY}
@@ -90,6 +90,6 @@
 		on:mousedown={() => handleMouseDown(MarkerType.IMPACT)}
 		on:mouseup={handleMouseUp}
 	>
-		<span class="absolute text-xs">{index + 1}</span>
+		<span class="absolute text-xs pointer-events-none">{index + 1}</span>
 	</button>
 {/if}
