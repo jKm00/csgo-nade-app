7cdd78da7b5e8a6822911b42a1512ee18d1a640b
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2023-06-23 21:28:09 +0200

    fix: lazy loading not working

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,9 +1,15 @@
+import { redirect } from '@sveltejs/kit';
+
 export const load = async ({ locals }) => {
 	const session = await locals.getSession();
 	const { data } = await locals.supabase
 		.from('profiles')
 		.select()
 		.eq('uuid', session?.user?.id);
+
+	if (session && data === null) {
+		throw redirect(302, '/profile-setup');
+	}
 
 	return {
 		session,
