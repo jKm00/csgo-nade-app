57defde1508772dbfe728e693b6fec1a8b653c5c
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2023-07-02 21:49:08 +0200

    wip: fetch strats from db

--- src/routes/(app)/strats/+page.ts
+++ src/routes/(app)/strats/+page.ts
@@ -1,10 +1,23 @@
-export const load = async ({ url }) => {
+export const load = async ({ url, parent }) => {
 	const map = url.searchParams.get('map');
 	const position = url.searchParams.get('position');
 	const side = url.searchParams.get('side');
 	const strat = url.searchParams.get('stratName');
 	const team = url.searchParams.get('teamName');
 	const author = url.searchParams.get('author');
+
+	const { supabase } = await parent();
+	const { data, error } = await supabase.rpc('query_strats_with_filters', {
+		p_map: map,
+		p_strat_position: position,
+		p_side: side,
+		p_strat_name: strat,
+		p_team_name: team,
+		p_author: author,
+	});
+
+	// TODO: Return data and display in .svelte
+	console.log(data, error);
 
 	return {
 		map,
