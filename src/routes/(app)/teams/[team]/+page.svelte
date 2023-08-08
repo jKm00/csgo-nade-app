05605ac487a97fec23239110ec74ee91c792b3e2
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2023-08-08 22:12:55 +0200

    fix: kick from team button

--- src/routes/(app)/teams/[team]/+page.svelte
+++ src/routes/(app)/teams/[team]/+page.svelte
@@ -184,29 +184,16 @@
 								<span class="md:hidden text-neutral-400">Joined at:</span
 								>{new Date(member.inserted_at).toLocaleDateString()}
 							</p>
-							{#if isTeamLeader && getProfile(member.profiles)?.uuid !== userId}
-								<div class="flex justify-between">
-									<a
-										class="max-md:hidden flex items-center gap-2 justify-end"
-										href={`/teams/${team.name}`}
-										>View player<svg
-											xmlns="http://www.w3.org/2000/svg"
-											height="1em"
-											viewBox="0 0 256 512"
-											><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
-												d="M246.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-9.2-9.2-22.9-11.9-34.9-6.9s-19.8 16.6-19.8 29.6l0 256c0 12.9 7.8 24.6 19.8 29.6s25.7 2.2 34.9-6.9l128-128z"
-											/></svg
-										></a
-									>
+							<div class="flex justify-between">
+								{#if isTeamLeader && getProfile(member.profiles)?.uuid !== userId}
 									<button
 										on:click|preventDefault={() => handleKick(member)}
-										class="text-sm text-red-400 hover:underline focus-within:underline"
+										class="text-sm text-red-400 hover:underline focus-within:underline w-full"
 										>Kick from team</button
 									>
-								</div>
-							{:else}
+								{/if}
 								<a
-									class="max-md:hidden flex items-center gap-2 justify-end"
+									class="max-md:hidden flex items-center gap-2 justify-end w-full"
 									href={`/teams/${team.name}`}
 									>View player<svg
 										xmlns="http://www.w3.org/2000/svg"
@@ -217,7 +204,7 @@
 										/></svg
 									></a
 								>
-							{/if}
+							</div>
 						</a>
 					{/each}
 				</div>
