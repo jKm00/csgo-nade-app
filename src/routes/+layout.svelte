f7350d7d8ba51cb794613cd560715850774d3cc3
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2023-10-10 19:22:56 +0200

    feat: support for cs2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,60 +1,60 @@
 <script lang="ts">
-	import Footer from '$lib/components/layout/Footer.svelte';
-	import { onMount } from 'svelte';
-	import '../app.css';
-	import Toast from '$lib/components/feedback/toast/Toast.svelte';
-	import { goto, invalidate } from '$app/navigation';
-	import type { User } from '$lib/features/navBar/types/User';
-	import { authUser } from '$lib/stores/authStore';
+  import Footer from '$lib/components/layout/Footer.svelte';
+  import { onMount } from 'svelte';
+  import '../app.css';
+  import Toast from '$lib/components/feedback/toast/Toast.svelte';
+  import { goto, invalidate } from '$app/navigation';
+  import type { User } from '$lib/features/navBar/types/User';
+  import { authUser } from '$lib/stores/authStore';
 
-	export let data;
+  export let data;
 
-	$: ({ supabase, session } = data);
+  $: ({ supabase, session } = data);
 
-	onMount(() => {
-		const { data } = supabase.auth.onAuthStateChange(
-			async (event, _session) => {
-				if (event === 'PASSWORD_RECOVERY') {
-					goto(`/login/new-password?token=${_session?.access_token}`);
-				} else {
-					if (_session?.expires_at !== session?.expires_at) {
-						invalidate('supabase:auth');
-					}
+  onMount(() => {
+    const { data } = supabase.auth.onAuthStateChange(
+      async (event, _session) => {
+        if (event === 'PASSWORD_RECOVERY') {
+          goto(`/login/new-password?token=${_session?.access_token}`);
+        } else {
+          if (_session?.expires_at !== session?.expires_at) {
+            invalidate('supabase:auth');
+          }
 
-					if (session) {
-						const { data } = await supabase
-							.from('profiles')
-							.select('id, uuid, name, username, profile_picture ( filename )')
-							.eq('uuid', session.user.id)
-							.single();
+          if (session) {
+            const { data } = await supabase
+              .from('profiles')
+              .select('id, uuid, name, username, profile_picture ( filename )')
+              .eq('uuid', session.user.id)
+              .single();
 
-						if (!data) {
-							goto('/profile-setup');
-						} else {
-							authUser.set(data as unknown as User);
-						}
-					} else {
-						authUser.set(null);
-					}
-				}
-			}
-		);
+            if (!data) {
+              goto('/profile-setup');
+            } else {
+              authUser.set(data as unknown as User);
+            }
+          } else {
+            authUser.set(null);
+          }
+        }
+      }
+    );
 
-		return () => data.subscription.unsubscribe();
-	});
+    return () => data.subscription.unsubscribe();
+  });
 </script>
 
 <svelte:head>
-	<link
-		rel="icon"
-		type="image/x-icon"
-		href="/assets/icons/favicons/favicon.ico"
-	/>
-	<title>CSGO Strats</title>
+  <link
+    rel="icon"
+    type="image/x-icon"
+    href="/assets/icons/favicons/favicon.ico"
+  />
+  <title>CS Strats</title>
 </svelte:head>
 
 <Toast />
 <div class="flex flex-col min-h-screen bg-neutral-900 text-white">
-	<slot />
-	<Footer />
+  <slot />
+  <Footer />
 </div>
