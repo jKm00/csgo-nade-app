89fe16eb049c936af930a5f60f8287bcca93e618
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2023-10-15 17:53:45 +0200

    version update

--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -4,7 +4,7 @@
 >
   <p>Joakim Edvardsen &copy; 2023. All rights reserved</p>
   <p>
-    Version 1.1.9 |
+    Version 1.1.10 |
     <a
       class="hover:underline focus-within:underline"
       href="/terms-of-service"
