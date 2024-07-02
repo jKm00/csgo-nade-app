fe966ae495d1468f6beafafc21df873ba1e32212
Joakim Edvardsen <joakimedvardsen2000@gmail.com>
2024-07-02 20:47:57 +0200

    fix: user menu icons

--- src/routes/(app)/users/[uuid]/settings/+layout.svelte
+++ src/routes/(app)/users/[uuid]/settings/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { ChevronLeft } from 'lucide-svelte';
   	import { type SettingsGroupRoute, useSettingsRoutes } from './routes.js';
 
 	export let data;
@@ -19,14 +20,7 @@
 		<a
 			class="flex items-center gap-2 text-sm mb-10 underline"
 			href="/users/{session?.user.id}"
-			><svg
-				class="w-2 fill-white"
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 320 512"
-				><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
-					d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"
-				/></svg
-			>Back to profile</a
+			><ChevronLeft />Back to profile</a
 		>
 		{#each routes as routeGroup}
 			<h2 class="text-muted-foreground font-bold uppercase text-sm mb-2">
