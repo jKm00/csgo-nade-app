29cca8bac3aeae405d179a3043405fd1fac6c71e
Joakim Edvardsen <77448888+jKm00@users.noreply.github.com>
2024-07-05 22:12:57 +0200

    feat: new strat card ui (#236)

--- src/lib/features/theme/Theme.svelte
+++ src/lib/features/theme/Theme.svelte
@@ -7,7 +7,7 @@
 </script>
 
 {#if dev}
-  <Button variant="outline" class="absolute bottom-4 right-4  " on:click={() => toggleMode()}><Sun class="w-4 h-4" /></Button>
+  <Button variant="outline" class="fixed bottom-4 right-4  " on:click={() => toggleMode()}><Sun class="w-4 h-4" /></Button>
 {/if}
 
 <ModeWatcher />
